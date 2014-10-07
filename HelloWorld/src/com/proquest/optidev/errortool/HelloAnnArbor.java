b56b89440e9c9d5191148cb7a5bb8e79dc8006d3
whe2014 <wenli.he@proquest.com>
2014-10-07 08:23:43 -0400

    Add property myName in HelloAnnArbor. Add method setName().
    Signed-off-by: whe2014 <wenli.he@proquest.com>

--- HelloWorld/src/com/proquest/optidev/errortool/HelloAnnArbor.java
+++ HelloWorld/src/com/proquest/optidev/errortool/HelloAnnArbor.java
@@ -1,13 +1,19 @@
 package com.proquest.optidev.errortool;
 
 public class HelloAnnArbor {
+	private String myName;
 
 	public HelloAnnArbor() {
 		// TODO Auto-generated constructor stub
+		myName = new String("Default Name") ;
 	}
 	
 	private String getName() {
 		return "Default Name" ;
+	}
+	
+	private void setName(String name) {
+		myName = name ;
 	}
 
 }
